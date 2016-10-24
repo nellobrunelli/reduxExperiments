1150f8eb8d9c0d536989363d9c14ca674f68ab0d
Nello Brunelli <nello.brunelli@wyscout.com>
2016-10-24 15:49:14 +0200

    webpack setup for reloading the app on the fly

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
 import BookList from '../containers/book-list';
 
-export default class App extends Component {
+export default class App extends React.Component {
     render() {
         return (
               <div><BookList /></div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,38 @@
+let webpack = require('webpack');
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
+    entry: ['./src/index.js'],
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015', 'stage-1']
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    },
     loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+        test: /\.jsx?$/,
+        exclude: /node_modules[\/\\]/,
+        loader: 'react-hot!babel-loader'
+    }],
+    plugins: [
+        new webpack.ProvidePlugin({React: 'react', ReactDOM: 'react-dom'}),
+        new webpack.HotModuleReplacementPlugin()
+        // new webpack.optimize.UglifyJsPlugin({minimize: true})
+    ]
 };
